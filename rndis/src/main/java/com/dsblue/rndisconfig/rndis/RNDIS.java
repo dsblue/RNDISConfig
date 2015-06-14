76e8a01a6af10e812b11898d7a4dabca83ea4ee2
Nathan Palmer <npalmer@camgian.com>
2015-06-13 22:39:11 -0500

    Change min API to 14 so the Switch is available; Fix margins; Add menu bar

--- rndis/src/main/java/com/dsblue/rndisconfig/rndis/RNDIS.java
+++ rndis/src/main/java/com/dsblue/rndisconfig/rndis/RNDIS.java
@@ -1,20 +1,43 @@
 package com.dsblue.rndisconfig.rndis;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.*;
 
 import java.io.*;
 
-public class RNDIS extends Activity {
+public class RNDIS extends AppCompatActivity {
 
     private static final String INTERFACE = "rndis0";
 
     private File usbTetherStart;
     private File usbTetherStop;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_rndi, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * Called when the activity is first created.
@@ -155,7 +178,7 @@ public class RNDIS extends Activity {
                 SharedPreferences.Editor prefsEditor = myPrefs.edit();
                 prefsEditor.putString("ip", ip.getText().toString());
                 prefsEditor.putString("mask", mask.getText().toString());
-                prefsEditor.commit();
+                prefsEditor.apply();
 
                 // true if the switch is in the On position
                 if (isChecked) {
